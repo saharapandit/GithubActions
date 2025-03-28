ac68532c219736e137063e15c6190db03d6253c5
saharapandit <160344569+saharapandit@users.noreply.github.com>
2025-03-28 18:42:54 -0500

    Update UnitTests.cs fixing intentional fail

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -6,7 +6,7 @@ public class Addition
 	[TestMethod]
 	public void Add_Valid_Patino()
 	{
-		Assert.AreEqual(4, Program.Add("1", "2"));
+		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
 	}
